e296a9db42f7a34580b75d9503e26681a3b0f124
ndukwon <ndukwon@gmail.com>
2018-07-12 01:06:01 +0900

    added MessageReplyReceiver

---
+++ BasicNotifications/Application/src/main/java/com/example/android/basicnotifications/MessageReplyReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.android.basicnotifications
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MessageReplyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("MessageReplyReceiver.onReceive() is not implemented")
+    }
+}
