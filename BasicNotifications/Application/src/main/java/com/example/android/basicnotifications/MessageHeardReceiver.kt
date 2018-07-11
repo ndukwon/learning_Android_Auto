fd39a52e04bb977e4bd1997ab8fd40ae68979750
ndukwon <ndukwon@gmail.com>
2018-07-12 01:02:14 +0900

    added MessageHeardReceiver

---
+++ BasicNotifications/Application/src/main/java/com/example/android/basicnotifications/MessageHeardReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.android.basicnotifications
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MessageHeardReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("MessageHeardReceiver.onReceive() is not implemented")
+    }
+}
