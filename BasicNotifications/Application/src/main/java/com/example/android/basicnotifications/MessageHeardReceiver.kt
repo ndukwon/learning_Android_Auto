53a0741761404f7c293ef726633cedbc3e15ec25
ndukwon <ndukwon@gmail.com>
2018-07-21 01:48:52 +0900

    added dummy receiver to print logs when receiving

--- BasicNotifications/Application/src/main/java/com/example/android/basicnotifications/MessageHeardReceiver.kt
+++ BasicNotifications/Application/src/main/java/com/example/android/basicnotifications/MessageHeardReceiver.kt
@@ -3,11 +3,14 @@ package com.example.android.basicnotifications
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 class MessageHeardReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        TODO("MessageHeardReceiver.onReceive() is not implemented")
+
+        val conversationId = intent.getIntExtra("conversation_id", -1)
+        Log.d("BasicNotifications", "MessageHeardReceiver conversation_id=$conversationId")
     }
 }
--- BasicNotifications/Application/src/main/java/com/example/android/basicnotifications/MessageReplyReceiver.kt
+++ BasicNotifications/Application/src/main/java/com/example/android/basicnotifications/MessageReplyReceiver.kt
@@ -3,11 +3,17 @@ package com.example.android.basicnotifications
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.support.v4.app.RemoteInput
+import android.util.Log
 
 class MessageReplyReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        TODO("MessageReplyReceiver.onReceive() is not implemented")
+        val conversationId = intent.getIntExtra("conversation_id", -1)
+
+        val replyText = RemoteInput.getResultsFromIntent(intent)?.getCharSequence("voice_reply_key")
+
+        Log.d("BasicNotifications", "Found voice reply [$replyText] from conversation")
     }
 }
